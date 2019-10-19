ad12749c99c1436149e7a541ec6dc4aea3a8777f
Oksana Lobko <ksuhaster@gmail.com>
2019-10-19 14:33:45 +0300

    переходное состояние на source.json и props

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,12 +3,14 @@ import './styles.scss';
 import { Header } from '../Components/Header';
 import { News } from '../Components/News';
 
+var source = require('./source.json');
+
 export const Home = () => {
   return (
     <>
         <h1>Home</h1>
         <Header />
-        <News />
+        <News source = {source} />
     </>
   )
 };
