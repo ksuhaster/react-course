26179b0b16e9c443ed368dd5f6947b2f9caab382
Oksana Lobko <ksuhaster@gmail.com>
2019-10-20 21:27:15 +0300

    промежуточное состояние, id у вопросов, глючит

--- src/Components/FAQ/index.js
+++ src/Components/FAQ/index.js
@@ -6,6 +6,7 @@ export const FAQ = (props) => {
     const questions = props.source.map((source_one, i) => (
         <Question
             key = { i }
+            id = { source_one.id }
             question = { source_one.question }
             answer = { source_one.answer }
         />
--- src/Components/Question/index.js
+++ src/Components/Question/index.js
@@ -2,26 +2,38 @@ import React, { useState } from 'react';
 import './styles.scss';
 
 export const Question = (props) => {
-  const [ counter, setCounter ] = useState(undefined);
 
-  const increase = () => {
-    console.log('increase ');
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  const [ currentOpen, setCurrentOpen ] = useState(undefined);
 
-  const decrease = () => {
-    console.log('decrease');
-    setCounter((prevCounter) => prevCounter - 1);
+  const openQuestion = (e) => {
+    let prevProfile = undefined;
+    console.log('e', e);
+    console.log('target.id', e.target.id);
+    setCurrentOpen(() => {
+      console.log("prev: ", prevProfile);
+      if (typeof prevProfile !== 'undefined') {
+        //close();
+        console.log('trying to close answer', e.target.id)
+      } else {
+        //("answer" + e.target.id).style = { display: 'show' };
+        console.log('trying to show answer', e.target.id);
+      }
+      prevProfile = e.target.id;
+      console.log("prev: ", prevProfile);
+      return e.target.id;
+    })
   }
 
   return (
     <>
-    <div className="question" onClick = { increase }>
+    <div className="question" onClick = {(e) => openQuestion(e) } id = {props.id}>
     { props.question }
-    { counter }
+    
+    { currentOpen }
+    
     <span className="question-sign">+</span>
     </div>
-    <div className="answer">{ props.answer }</div>
+    <div className="answer" id = { "answer" + props.id }>{ props.answer }</div>
     </>
   )
 };
