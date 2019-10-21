d1a95775459e5055ff99cf679852641900645edb
Oksana Lobko <ksuhaster@gmail.com>
2019-10-21 20:06:04 +0300

    работающий вариант, но одной компонентой

--- src/Components/FAQ/index.js
+++ src/Components/FAQ/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
-import { Question } from '../Question';
 
 export const FAQ = (props) => {
+
+    const [ whoOpen, setWhoOpen ] = useState(false);
+
+    const clickQuestion = (i) => {
+      setWhoOpen(() => {
+        if (i === whoOpen) {
+            return undefined;  // same question, close
+        } else {
+            return i;
+        };
+      })
+    }
+
     const questions = props.source.map((source_one, i) => (
-        <Question
-            key = { i }
-            id = { source_one.id }
-            question = { source_one.question }
-            answer = { source_one.answer }
-        />
+        <>
+        <div className="question"
+                key = { 'question' + i }
+                onClick = {() => clickQuestion(i) }>
+            { source_one.question }
+            <span className="question-sign">
+                {(whoOpen === i ? '-' : '+')}
+            </span>
+        </div>
+
+        <div className={"answer " + (whoOpen === i ? 'answer-show' : 'answer-hide')}
+            key = { "answer" + i }>
+            { source_one.answer }
+        </div>
+        </>
     ));
 
     return (
