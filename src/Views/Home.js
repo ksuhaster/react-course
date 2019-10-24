b05f18983071320a129795124f63137fc51c840d
Oksana Lobko <ksuhaster@gmail.com>
2019-10-24 12:35:40 +0300

    выключила FAQ

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './styles.scss';
 import { Header } from '../Components/Header';
 import { News } from '../Components/News';
-import { FAQ } from '../Components/FAQ';
 
 var source = require('./source.json');
-var sourceFAQ = require('./sourceFAQ.json');
 
 export const Home = () => {
   return (
@@ -13,8 +11,6 @@ export const Home = () => {
         <h1>Home</h1>
         <Header />
         <News source = {source} />
-
-        <FAQ source = { sourceFAQ } />
     </>
   )
 };
