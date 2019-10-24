0bf03cd4cb27290e4e902b906ed51fa3308f9015
Oksana Lobko <ksuhaster@gmail.com>
2019-10-24 18:28:35 +0300

    с useEffect

--- src/Components/News/index.js
+++ src/Components/News/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.scss';
 import { Article } from '../Article';
 import { useNews } from './useNews';
 
 export const News = () => {
   const { posts } = useNews();
+  const [showLoader, setShowLoader] = useState(true);
 
   const articles = posts.map((source_one) => (
     <Article
@@ -13,10 +14,16 @@ export const News = () => {
     />
   ));
 
+  useEffect(() => {
+    if (posts.length > 0) {
+      setShowLoader(false);
+    }
+  }, [posts]);
+
   return (
     <>
       <div className="grey">
-        <span className={(posts.length === 0 ? 'loader-show' : 'loader-hide')}>Загрузка данных...</span>
+        <span className={(showLoader ? 'loader-show' : 'loader-hide')}>Загрузка данных...</span>
         { articles }
       </div>
     </>
