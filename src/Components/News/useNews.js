875290cba4cba0b17304e50c161ed3ab06cbd69f
Oksana Lobko <ksuhaster@gmail.com>
2019-10-29 16:20:08 +0200

    правильное использование LocalStorage, но пока без запоминания времени

--- src/Components/News/useNews.js
+++ src/Components/News/useNews.js
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 import { api } from './api';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useNews = () => {
     const [ posts, setPosts ] = useLocalStorage('postsCache', []);
 
-    useEffect(() => {
+    if (posts.length === 0) {
+        console.log('go for the posts');
         (async () => {
             const posts = await api.getPosts();
             setPosts(posts);
         })();
-    }, []);
+    }
 
     return { posts };
 }
