7bf182275b71348c7e42fd5df61196e8b5e9e577
Oksana Lobko <ksuhaster@gmail.com>
2019-10-29 16:27:29 +0200

    с сохранением времени и кешом на 10мин

--- src/Components/News/useNews.js
+++ src/Components/News/useNews.js
@@ -4,12 +4,15 @@ import { useLocalStorage } from './useLocalStorage';
 
 export const useNews = () => {
     const [ posts, setPosts ] = useLocalStorage('postsCache', []);
+    const [ postsTime, setPostsTime ] = useLocalStorage('postsTime', null);
 
-    if (posts.length === 0) {
+    if (posts.length === 0 || postsTime === null ||
+            (Date.now() - postsTime) > 10*60*1000) {
         console.log('go for the posts');
         (async () => {
             const posts = await api.getPosts();
             setPosts(posts);
+            setPostsTime(Date.now());
         })();
     }
 
