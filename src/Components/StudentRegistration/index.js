ad683ddddaa3c7c429b5e5806f18653b02aeeba5
Oksana Lobko <ksuhaster@gmail.com>
2019-11-06 19:40:43 +0200

    yup validation

--- src/Components/StudentRegistration/index.js
+++ src/Components/StudentRegistration/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './styles.scss';
 import { MyTextInput } from './inputs/myTextInput';
 import { MySelect } from './inputs/mySelect';
@@ -16,6 +17,37 @@ export const StudentRegistration = () => {
     sex: '',
     speciality: ''
   };
+
+  const validationSchema = Yup.object().shape({
+      firstName: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+      surname: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+      age: Yup.number()
+          .min(6)
+          .max(60),
+      email: Yup.string()
+          .email('Invalid email')
+          .required('Required'),
+      sex: Yup.string()
+          .oneOf([
+              'male',
+              'female'
+          ], 'Invalid Sex')
+          .required('Required'),
+      speciality: Yup.string()
+          .oneOf([
+              'designer',
+              'developer',
+              'manager'
+          ], 'Invalid Speciality')
+          .required('Required'),
+  });
+
   const [ studentData, setStudentData ] = useLocalStorage('student', initialValues);
   const [ formIsNew, setFormIsNew ] = useState(JSON.stringify(initialValues) === JSON.stringify(studentData));
 
@@ -26,50 +58,13 @@ export const StudentRegistration = () => {
     history.push({pathname: book.student})
   };
 
-  const validateForm = (values) => {
-      let errors = {};
-
-      if(!values.firstName || typeof values.firstName !== 'string') {
-          errors.firstName = 'Required text';
-      }
-
-      if(!values.surname) {
-          errors.surname = 'Required text';
-      }
-
-      if (values.age) {
-          try {
-              let age = parseInt(values.age);
-              if (typeof age !== 'number' || age < 6 || age > 60) {
-                  errors.age = 'Should be number 6-60';
-              }
-          } catch {
-              errors.age = 'Should be number 6-60';
-          }
-      }
-
-      if(!values.email || !(/\S+@\S+\.\S+/).test(values.email) ) {
-          errors.email = 'Required email';
-      }
-
-      if(!values.sex) {
-          errors.sex = 'Required sex';
-      }
-
-      if(!values.speciality) {
-          errors.speciality = 'Required';
-      }
-
-      return errors;
-  };
-
   return (
     <section className='customer'>
       <h1>Student Registration</h1>
       <Formik
         initialValues={ studentData }
         onSubmit={ submitForm }
-        validate={ validateForm }
+        validationSchema={ validationSchema }
       >
       {({errors, touched}) => {
         return (
